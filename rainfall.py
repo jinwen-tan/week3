ac5a5d49953ff959ee8a20c23cca1c50d9c85fb8
jinwen-tan <tanjinwen@graduate.utm.my>
2022-11-02 10:45:57 +0800

    combined int and float

--- rainfall.py
+++ rainfall.py
@@ -1,12 +1,18 @@
-from imaplib import Int2AP
-
-
+#get input
 inches_str = input("How many inches of ran have falllen: ")
+
+#convert to integer/float
 inches_float = float(inches_str)
-volume = (inches_float/12)*43560
-gallons = volume * 7.48051945
-gallons_int= int(gallons)
 inches_int = int(inches_float)
-inches_float = float(inches_str)
-print(inches_float, " in. rain on 1 acre is", gallons, "gallons")
+
+
+#volume of int and float calculate separately
+volume_int = (inches_int/12)*43560
+volume_float = (inches_float/12)*43560
+
+#gallons of int and float calculate separately 
+gallons_int = volume_int * 7.48051945
+gallons_float = volume_float * 7.48051945
+
 print(inches_int, " in. rain on 1 acre is", gallons_int, "gallons")
+print(inches_float, " in. rain on 1 acre is", gallons_float, "gallons")
